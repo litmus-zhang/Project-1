0cfdb04b9825354cda64744ac26c9a9a660fbc7c
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-04-04 17:31:14 +0100

    Done

--- client/src/App.js
+++ client/src/App.js
@@ -5,60 +5,9 @@ import Token from "./contracts/Token.sol/Token.json";
 import "./App.css";
 
 function App() {
-  const [ethBalance, setEthBalance] = useState(0);
-  const [balance, setBalance] = useState(0);
-  const [methods, setMethods] = useState({});
-  const [address, setAddress] = useState("");
-  const [amount, setAmount] = useState("");
-
-  useEffect(() => {
-    const getData = async () => {
-      const provider = await getEthers();
-      const signer = provider.getSigner();
-      const address = signer.getAddress();
-      const bal = await provider.getBalance(address);
-      const balance = utils.formatEther(bal);
-      const contract = new Contract(
-        "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-        Token.abi,
-        provider
-      );
-      setMethods(contract.connect(signer));
-      const tokenBalance = (await contract.balanceOf(address)).toString();
-      setBalance(tokenBalance);
-      setEthBalance(balance);
-      contract.on("Transfer", async (from, to, amount, event) => {
-        const bal = await provider.getBalance(address);
-        const balance = utils.formatEther(bal);
-        const tokenBalance = (await contract.balanceOf(address)).toString();
-        setBalance(tokenBalance);
-        setEthBalance(balance);
-      });
-    };
-
-    getData().then(() => {
-      console.log("Data has been obtained");
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <div className="centralDiv">
-        <p className="balance">ETH Balance: {ethBalance}</p>
-        <p className="balance">Token Balance: {balance}</p>
-        <input className="input" onChange={(e) => setAddress(e.target.value)} />
-        <input className="input" onChange={(e) => setAmount(e.target.value)} />
-        <button
-          className="button"
-          onClick={async () => {
-            await methods.transfer(address, amount);
-          }}
-        >
-          Transfer
-        </button>
-      </div>
-    </div>
-  );
+    <div>Nothing here yet</div>
+  )
 }
 
 export default App;
