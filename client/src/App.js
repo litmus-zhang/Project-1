2b06f15415b6fabef4cb473f2d68ef9d519817c7
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-04-05 10:21:01 +0100

    Installed materialUI and updated App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
-import getEthers from "./getEthers";
-import { Contract, utils } from "ethers";
-import Token from "./contracts/Token.sol/Token.json";
+// import getEthers from "./getEthers";
+// import { Contract, utils } from "ethers";
+// import Token from "./contracts/Token.sol/Token.json";
 import "./App.css";
+import Box from "@mui/material/Box";
+import NavBar from "./components/navbar";
+import SideBar from "./components/sidebar";
 
 function App() {
   return (
-    <div>Nothing here yet</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <NavBar />
+      <SideBar />
+    </Box>
+  );
 }
 
 export default App;
